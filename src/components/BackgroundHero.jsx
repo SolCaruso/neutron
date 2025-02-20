4a366bc00352c91031ac12d34cb1f3840e8e00fe
SolCaruso <caruso@ocelot.ltd>
2025-02-19 23:57:38 -0500

    adjusts hero image

--- src/components/BackgroundHero.jsx
+++ src/components/BackgroundHero.jsx
@@ -1,4 +1,3 @@
-// components/BackgroundHero.jsx
 import Image from "next/image";
 
 export default function BackgroundHero() {
@@ -11,28 +10,23 @@ export default function BackgroundHero() {
         -translate-x-1/2 
         pointer-events-none
         overflow-hidden
-        w-[1064px] 
-        h-[385px]
-        max-w-full
+        w-full 
+        sm:w-[1064px] 
+        h-[420px]
+        sm:h-[385px]
         bg-no-repeat 
         bg-top
         z-10
-        [background:radial-gradient(
-          50%_50%_at_50%_-20%,
-          rgba(255,99,99,0.08)_0,
-          hsla(0,100%,73%,0.04)_50.5%,
-          rgba(90,23,177,0)_100%
-        )]
-        [background-size:clamp(200px,calc(100vw_*_1.586_+_45.31px),600px)]
+        [background:radial-gradient(50%_50%_at_50%_-20%,rgba(255,99,99,0.08)_0,hsla(0,100%,73%,0.04)_50.5%,rgba(90,23,177,0)_100%)]
+        [background-size:clamp(200px,calc(100vw*1.586+45.31px),600px)]
       "
     >
       <Image
         src="/bg/bg-hero.webp"
         alt="Neutron red background"
-        width={1064}
-        height={385}
+        fill
         priority
-        className="object-cover w-full h-auto"
+        className="object-cover"
       />
     </div>
   );
