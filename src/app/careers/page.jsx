3618936fd6cb163b0745c0d0fa2ef040a8f0e3be
SolCaruso <caruso@ocelot.ltd>
2025-02-23 21:29:26 -0500

    adds careers images

--- src/app/careers/page.jsx
+++ src/app/careers/page.jsx
@@ -1,5 +1,6 @@
-import ModalTrigger from '@/components/careers/ModalTrigger'
-import Faq from '@/components/careers/Faq'
+import AnimatedImage from '@/components/careers/AnimatedImage';
+import ModalTrigger from '@/components/careers/ModalTrigger';
+import Faq from '@/components/careers/Faq';
 
 export default function HomePage() {
   return (
@@ -25,51 +26,57 @@ export default function HomePage() {
                     and real-world impact.
                   </p>
                   <div className="mt-10 flex">
-                    {/* Renders our client-side modal trigger */}
                     <ModalTrigger />
                   </div>
                 </div>
 
-                {/* Images (example) */}
+                {/* Images */}
                 <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
                   <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
-                    <img
-                      alt=""
-                      src="https://images.unsplash.com/photo-1670272502246-768d249768ca?ixlib=rb-4.0.3&auto=format&fit=crop&w=1152&q=80"
-                      className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                    <AnimatedImage
+                      alt="Office Space"
+                      src="/careers/office.webp"
+                      width={700}
+                      height={500}
+                      className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl object-cover"
                     />
                   </div>
                   <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
                     <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
-                      <img
-                        alt=""
-                        src="https://images.unsplash.com/photo-1605656816944-971cd5c1407f?ixlib=rb-4.0.3&auto=format&fit=crop&w=768&h=604&q=80"
-                        className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                      <AnimatedImage
+                        alt="EV Car"
+                        src="/careers/ev.webp"
+                        width={384}
+                        height={288}
+                        className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl object-cover"
                       />
                     </div>
                     <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
-                      <img
-                        alt=""
-                        src="https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-4.0.3&auto=format&fit=crop&w=1152&h=842&q=80"
-                        className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                      <AnimatedImage
+                        alt="Radio Control"
+                        src="/careers/radio.webp"
+                        width={740}
+                        height={528}
+                        className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl object-cover"
                       />
                     </div>
                     <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
-                      <img
-                        alt=""
-                        src="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&auto=format&fit=crop&w=768&h=604&q=80"
-                        className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                      <AnimatedImage
+                        alt="Neutron Board"
+                        src="/careers/board.webp"
+                        width={384}
+                        height={288}
+                        className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl object-cover"
                       />
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            {/* FAQ section */}
             <Faq />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
